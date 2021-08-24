9bad0db0288834000cd41f6b1f556410a661b4cb
Tamas328 <51370889+Tamas328@users.noreply.github.com>
2021-08-24 21:15:41 +0300

    Done

--- src/router/index.js
+++ src/router/index.js
@@ -3,40 +3,41 @@ import Home from "@/components/Home.vue";
 import About from "@/components/About.vue";
 import Login from "@/components/Login.vue";
 import Register from "@/components/Register.vue";
-import store from "../vuex";
+// import store from "../vuex";
 
-const routes = [{
-        path: "/",
-        name: "Home",
-        component: Home,
-    },
-    {
-        path: "/about",
-        name: "About",
-        component: About,
-        beforeEnter: (to, from, next) => {
-            if (store.state.user.loggedIn == false) {
-                next('/login');
-            } else {
-                next();
-            }
-        }
-    },
-    {
-        path: '/login',
-        name: 'login',
-        component: Login,
-    },
-    {
-        path: '/register',
-        name: 'register',
-        component: Register,
-    }
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/about",
+    name: "About",
+    component: About,
+    // beforeEnter: (to, from, next) => {
+    //     if (store.state.user.loggedIn == false) {
+    //         next('/login');
+    //     } else {
+    //         next();
+    //     }
+    // }
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: Login,
+  },
+  {
+    path: "/register",
+    name: "register",
+    component: Register,
+  },
 ];
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes,
+  history: createWebHistory(),
+  routes,
 });
 
 export default router;
