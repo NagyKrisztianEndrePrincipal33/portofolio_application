e24281e23f48a87a51fe37b7f2d979f0c0b8adf3
Tamas328 <51370889+Tamas328@users.noreply.github.com>
2021-08-24 21:34:15 +0300

    Fixed index.js

--- src/router/index.js
+++ src/router/index.js
@@ -5,38 +5,38 @@ import Login from "@/components/Login.vue";
 import Register from "@/components/Register.vue";
 import ProfilePage from "@/components/ProfilePage.vue";
 import EditPage from "@/components/EditPage.vue";
-import store from "../vuex";
 
-const routes = [{
-        path: "/",
-        name: "Home",
-        component: Home,
-    },
-    {
-        path: "/about",
-        name: "About",
-        component: About,
-    },
-    {
-        path: '/login',
-        name: 'login',
-        component: Login,
-    },
-    {
-        path: '/register',
-        name: 'register',
-        component: Register,
-    },
-    {
-        path: '/CV/:webid',
-        name: 'profile_page',
-        component: ProfilePage
-    },
-    {
-        path: '/edit/:webid',
-        name: 'edit_page',
-        component: EditPage
-    }
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/about",
+    name: "About",
+    component: About,
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: Login,
+  },
+  {
+    path: "/register",
+    name: "register",
+    component: Register,
+  },
+  {
+    path: "/CV/:webid",
+    name: "profile_page",
+    component: ProfilePage,
+  },
+  {
+    path: "/edit/:webid",
+    name: "edit_page",
+    component: EditPage,
+  },
 ];
 
 const router = createRouter({
