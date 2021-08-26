fbfe678ad7b7d2a0bb0f65efaa7188c3d182d2ca
bakolaszlo <bakolaszlo999@gmail.com>
2021-08-26 12:55:47 +0300

    Added basic loading

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,14 +2,27 @@
   <navigation-bar :user="user"></navigation-bar>
   <div class="body">
     <p v-if="user.loggedIn">You are logged in!{{ user.data }}</p>
+    <div v-if="user.loggedIn">
+      <div v-if="newsFeed">
+        <div v-for="news in newsFeed" :key="news">
+          {{ news }}
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import NavigationBar from "./NavigationBar";
 import { mapGetters } from "vuex";
+import firebase from "../database/firebase";
 export default {
   name: "Home",
+  data() {
+    return {
+      newsFeed: [],
+    };
+  },
   components: {
     NavigationBar,
   },
@@ -17,6 +30,21 @@ export default {
     ...mapGetters({
       user: "user",
     }),
+  },
+  mounted() {
+    console.log("Loaded");
+    firebase
+      .firestore()
+      .collection("users")
+      //.orderBy("editedAt")
+      .limit(5)
+      .get()
+      .then((doc) => {
+        doc.forEach((doc) => {
+          this.newsFeed.push(doc.data());
+          console.log(doc.data());
+        });
+      });
   },
 };
 </script>
