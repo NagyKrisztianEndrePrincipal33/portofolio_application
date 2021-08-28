2999e5d60f70369c11d8083528cc360286835a6b
bakolaszlo <bakolaszlo999@gmail.com>
2021-08-28 13:30:32 +0300

    Added loading (the css is like that for visual purposes)

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,8 +4,16 @@
     <p v-if="user.loggedIn">You are logged in!{{ user.data }}</p>
     <div v-if="user.loggedIn">
       <div v-if="newsFeed">
-        <div v-for="news in newsFeed" :key="news">
-          {{ news }}
+        <div class="person" v-for="news in newsFeed" :key="news">
+          <div class="name">
+            {{ news.lastName + " " + news.firstName }} just edited
+            <div v-if="news.gender == male">his</div>
+            <div v-else>her</div>
+          </div>
+          <div v-for="editField in lastEdited.slice(0, 3)" :key="editField">
+            {{ editField }}
+          </div>
+          <div v-if="lastEdited.length > 3">and more! check it out.</div>
         </div>
       </div>
     </div>
@@ -21,6 +29,7 @@ export default {
   data() {
     return {
       newsFeed: [],
+      lastEdited: [],
     };
   },
   components: {
@@ -31,17 +40,18 @@ export default {
       user: "user",
     }),
   },
-  mounted() {
+  created() {
     console.log("Loaded");
     firebase
       .firestore()
       .collection("users")
-      //.orderBy("editedAt")
+      .orderBy("editedAt")
       .limit(5)
       .get()
       .then((doc) => {
         doc.forEach((doc) => {
           this.newsFeed.push(doc.data());
+          this.lastEdited = doc.data().lastEdited;
           console.log(doc.data());
         });
       });
@@ -51,5 +61,16 @@ export default {
 <style scoped lang="scss">
 .body {
   padding-top: 70px;
+}
+
+.person {
+  display: block;
+  justify-content: space-between;
+  background-color: blue;
+}
+
+.name {
+  background-color: blueviolet;
+  padding-top: 12px;
 }
 </style>
